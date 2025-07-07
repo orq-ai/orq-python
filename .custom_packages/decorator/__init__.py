3788f6ceb84cf74003755d617313134b21423248
Harold James Quilang <harold@experience.digital>
2025-07-07 21:28:39 +0800

    refactor: clean up (TOPS-839)

--- .custom_packages/decorator/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Orq Python Decorator SDK for distributed tracing."""
-
-from decorator.decorators import traced
-from decorator.types import SpanType
-from decorator.config import init
-
-__all__ = ["init", "traced", "SpanType"]
